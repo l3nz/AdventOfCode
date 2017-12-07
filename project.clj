f0a0694a27fc4550de6f8a1242255a5f78320248
lenz <lenz@lenzs-MacBook-Pro.local>
2017-12-07 13:36:35 +0100

    Housekeping

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject AdventOfCode "2017"
+  :aot :all
+
+  :plugins [[lein-gorilla "0.4.0"]]
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [lein-gorilla "0.4.0"]
+])
+
+
